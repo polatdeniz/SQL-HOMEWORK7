64641382a91375137934d07f84419c3900a62d1f
Deniz POLAT <101480115+polatdeniz@users.noreply.github.com>
2022-08-31 01:01:29 +0300

    Create homework7.sql

---
+++ homework7.sql
@@ -0,0 +1,4 @@
+SELECT rating, COUNT(title) FROM film GROUP BY rating;
+SELECT replacement_cost, COUNT(replacement_cost) FROM film GROUP BY replacement_cost HAVING COUNT(replacement_cost) > 50;
+SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
